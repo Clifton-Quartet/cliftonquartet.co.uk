14ae85bfcc76228d7578c5a827051eff326f7e45
Sergio Correia <sergiosdcorreia@gmail.com>
2025-05-15 22:02:20 +0100

    Added arrow up to top of the pages in footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,18 @@
+"use client";
+
+import { ArrowBigUp } from "lucide-react";
+
 const Footer = () => {
+  const scrollToTop = (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 items-center m-4">
+    <div className="relative grid grid-cols-1 md:grid-cols-3 items-center m-4">
       <div></div>
       <p className="text-center text-white text-sm">
         © 2025 The Clifton String Quartet
@@ -15,6 +27,13 @@ const Footer = () => {
           Sergio Correia
         </a>
       </p>
+      <button
+        onClick={scrollToTop}
+        className="absolute right-4 bottom-12 w-10 h-10 bg-slate-900 text-[#fdfe6c] flex justify-center items-center rounded-full hover:bg-slate-500 cursor-pointer"
+        aria-label="Scroll to top"
+      >
+        <ArrowBigUp size={24} />
+      </button>
     </div>
   );
 };

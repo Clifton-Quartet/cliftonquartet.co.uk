ca1f807305c820642728a739994390a39d25fad9
Sergio Correia <sergiosdcorreia@gmail.com>
2025-05-15 21:49:19 +0100

    Added link to my website in the footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,19 @@
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center m-4">
+    <div className="grid grid-cols-1 md:grid-cols-3 items-center m-4">
+      <div></div>
       <p className="text-center text-white text-sm">
         © 2025 The Clifton String Quartet
+      </p>
+      <p className="text-right text-white text-sm">
+        Designed and Developed by{" "}
+        <a
+          href="https://www.sergiocorreia.dev"
+          target="_blank"
+          className="text-[#fdfe6c]"
+        >
+          Sergio Correia
+        </a>
       </p>
     </div>
   );

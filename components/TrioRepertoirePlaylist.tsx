6f9e621760511dec583e130959eec329fe452efc
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-05-20 16:01:37 +0100

    Styling for the repertoires

--- components/TrioRepertoirePlaylist.tsx
+++ components/TrioRepertoirePlaylist.tsx
@@ -367,7 +367,7 @@ const TrioRepertoirePlaylist: React.FC<RepertoirePlaylistProps> = ({
               <input
                 type="text"
                 placeholder="Search by title or composer..."
-                className="w-full pl-10 pr-4 py-2 border text-slate-900 border-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-500"
+                className="w-full pl-10 pr-4 py-2 border text-yellow-100 border-yellow-100 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-500"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
               />

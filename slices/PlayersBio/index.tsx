6e6bc0c28dec0bcd040d80cca88b85ca002cb16c
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-06-04 22:29:45 +0100

    Adjustments on layout

--- slices/PlayersBio/index.tsx
+++ slices/PlayersBio/index.tsx
@@ -17,42 +17,44 @@ const PlayersBio: FC<PlayersBioProps> = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="overflow-hidden min-h-full font-sans leading-loose text-yellow-900"
+      className="overflow-hidden font-sans leading-loose text-yellow-900"
       style={{
         backgroundColor: slice.primary.background_color || undefined,
       }}
     >
-      <div className="p-8 lg:p-16">
-        <SlideIn>
-          <div className="font-serif uppercase font-bold tracking-widest text-3xl lg:text-5xl mb-4">
-            <PrismicRichText field={slice.primary.title} />
-          </div>
-        </SlideIn>
-        <SlideIn>
-          <div className="text-xl lg:text-2xl">
-            <PrismicRichText field={slice.primary.sub_heading} />
-          </div>
-        </SlideIn>
-      </div>
-      <div className="p-8 lg:p-16 flex flex-col sm:grid sm:grid-cols-2 xl:grid-cols-4 justify-center gap-16">
-        {slice.primary.player_bio.map((item, index) => (
-          <SlideIn key={index}>
-            <div className="flex flex-col gap-4 justify-center">
-              <div
-                className="w-full aspect-square bg-cover bg-center bg-no-repeat"
-                style={{
-                  backgroundImage: `url(${item.player_photo.url})`,
-                }}
-              ></div>
-              <div className="text-lg">
-                <div className="text-2xl font-bold">
-                  <PrismicRichText field={item.player_name} />
-                </div>
-                <PrismicRichText field={item.player_bio} />
-              </div>
+      <div className="min-h-[100vh] flex flex-col justify-center">
+        <div className="p-8 lg:p-16">
+          <SlideIn>
+            <div className="font-serif uppercase font-bold tracking-widest text-3xl lg:text-5xl mb-4">
+              <PrismicRichText field={slice.primary.title} />
             </div>
           </SlideIn>
-        ))}
+          <SlideIn>
+            <div className="text-xl lg:text-2xl">
+              <PrismicRichText field={slice.primary.sub_heading} />
+            </div>
+          </SlideIn>
+        </div>
+        <div className="p-8 lg:p-16 flex flex-col sm:grid sm:grid-cols-2 xl:grid-cols-4 justify-center gap-16">
+          {slice.primary.player_bio.map((item, index) => (
+            <SlideIn key={index}>
+              <div className="flex flex-col gap-4 justify-center">
+                <div
+                  className="w-full aspect-square bg-cover bg-center bg-no-repeat"
+                  style={{
+                    backgroundImage: `url(${item.player_photo.url})`,
+                  }}
+                ></div>
+                <div className="text-lg">
+                  <div className="text-2xl font-bold">
+                    <PrismicRichText field={item.player_name} />
+                  </div>
+                  <PrismicRichText field={item.player_bio} />
+                </div>
+              </div>
+            </SlideIn>
+          ))}
+        </div>
       </div>
       <ContactForm />
     </section>
